0bc59f00b93aad883b487be755f3af1714718341
Ivan Paraskun <ig.paraskun@gmail.com>
2024-09-03 10:55:50 +0700

    euler updated.

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,21 @@
-fn main() {}
+use algo::math::ode::euler;
+
+fn derivative(t: f64, y: f64) -> f64 {
+    2.0 * t * y
+}
+
+fn main() {
+    let step = 0.5;
+
+    let mut grid = Vec::<euler::Point>::new();
+    let mut t = 0.0;
+
+    grid.push(euler::Point(t, 1.0));
+
+    for _i in 1..10 {
+        t += step;
+        grid.push(euler::Point(t, 0.0));
+    }
+
+    euler::solve(derivative, &mut grid[..], euler::Scheme::FES)
+}
--- src/math/ode/euler.rs
+++ src/math/ode/euler.rs
@@ -4,22 +4,35 @@ pub enum Scheme {
     TES,
 }
 
-pub struct Point(f64, f64);
+pub struct Point(pub f64, pub f64);
 
-pub fn solve(grid: &mut [Point], d: fn(f64, f64) -> f64, scheme: Scheme) {
+pub fn solve(d: fn(f64, f64) -> f64, grid: &mut[Point], scheme: Scheme) {
     if grid.len() < 2 {
         return;
     }
 
     let step = grid[1].0 - grid[0].0;
 
-    match scheme {
-        Scheme::FES => {
-            for i in 1..grid.len() {
-                grid[i].1 = grid[i - 1].1 + step * d(grid[i - 1].0, grid[i - 1].1);
+    for i in 1..grid.len() {
+        grid[i].1 = match scheme {
+            Scheme::FES => grid[i - 1].1 + step * d(grid[i - 1].0, grid[i - 1].1),
+            Scheme::SES => {
+                grid[i - 1].1
+                    + step / 2.0
+                        * (d(grid[i - 1].0, grid[i - 1].1)
+                            + d(
+                                grid[i].0,
+                                grid[i - 1].1 + step * d(grid[i - 1].0, grid[i - 1].1),
+                            ))
+            }
+            Scheme::TES => {
+                grid[i - 1].1
+                    + step
+                        * d(
+                            grid[i / 2].0,
+                            grid[i - 1].1 + step / 2.0 * d(grid[i - 1].0, grid[i - 1].1),
+                        )
             }
         }
-        Scheme::SES => {}
-        Scheme::TES => {}
     }
 }
