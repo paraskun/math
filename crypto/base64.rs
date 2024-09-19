31bd2b6581c6b41237c4056d7ec48b8f334c2f7d
ig.paraskun@gmail.com <ig.paraskun@gmail.com>
2024-09-19 21:31:36 +0700

    upd.

--- crypto/base64.rs
+++
@@ -1,5 +0,0 @@
-fn encode(x: &[u8]) -> Box<[u8]> {
-    return Box::new([]);
-}
-
-fn main() {}
--- crypto/huff/huff.h
+++
@@ -1,9 +0,0 @@
-#ifndef HUFF_H
-#define HUFF_H
-
-#include <stdio.h>
-
-int encode(FILE *src, FILE *dst);
-int decode(FILE *src, FILE *dst);
-
-#endif // HUFF_H
