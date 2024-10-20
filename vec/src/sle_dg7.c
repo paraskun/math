7c6a1bc040ee19b0b3b9a07bef09372887c1b9fa
ig.paraskun@gmail.com <ig.paraskun@gmail.com>
2024-10-20 16:34:18 +0700

    upd.

--- vec/src/sle_dg7.c
+++ vec/src/sle_dg7.c
@@ -1,13 +1,34 @@
 #include <sle_dg7.h>
 
-int sle_dg7_jac_dp(struct mtx_dg7* mp, struct vec* xp, struct vec* fp) {
+typedef int (*fn)(struct args*, real*);
+
+static int iter(struct args* a, fn f, real* d, real om) {
+  int n = a->xp->n;
+
+  real* xvp = a->xp->v;
+  real* mdp = a->mp->dp;
+
+  f(a, d);
+
+  for (int i = 0; i < n; ++i)
+    xvp[i] = xvp[i] + (om / mdp[i]) * d[i];
+
   return 0;
 }
 
-int sle_dg7_rx(struct mtx_dg7* mp, struct vec* xp, struct vec* fp) {
+static int jac(struct args* a, real* d) {
+  int n = a->xp->n;
+
+  real* fvp = a->fp->v;
+  real* xvp = a->xp->v;
+
+  for (int j = 0; j < n; ++j) {}
+
   return 0;
 }
 
-int sle_dg7_blk_rx(struct mtx_dg7* mp, struct vec* xp, struct vec* fp) {
-  return 0;
-}
+int sle_dg7_jac(struct args, struct props) {}
+
+int sle_dg7_rx(struct args, struct props);
+
+int sle_dg7_brx(struct args, struct props);
--- vec/src/sle_dg7.h
+++ vec/src/sle_dg7.h
@@ -4,10 +4,24 @@
 #include <mtx_dg7.h>
 #include <vec.h>
 
-int sle_dg7_jac_dp(struct mtx_dg7* mp, struct vec* xp, struct vec* fp);
+struct args {
+  struct mtx_dg7* mp;
 
-int sle_dg7_rx(struct mtx_dg7* mp, struct vec* xp, struct vec* fp);
+  struct vec* xp;
+  struct vec* fp;
+};
 
-int sle_dg7_blk_rx(struct mtx_dg7* mp, struct vec* xp, struct vec* fp);
+struct props {
+  real omg;
+  real eps;
+
+  int mk;
+};
+
+int sle_dg7_jac(struct args, struct props);
+
+int sle_dg7_rx(struct args, struct props);
+
+int sle_dg7_brx(struct args, struct props);
 
 #endif  // SLE_DG7_H
